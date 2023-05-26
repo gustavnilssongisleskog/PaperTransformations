2207b5b8afdc1718d81fe46353e07591586ff985
Gustav Nilsson Gisleskog <gisleskoggustav@gmail.com>
2023-05-26 02:07:06 +0200

    can now "rotate" the paper to simulate a picture taken of the paper straight on

--- src/playground.py
+++ src/playground.py
@@ -4,8 +4,9 @@ from numpy import ndarray
 from typing import Tuple
 from matrices import *
 from matplotlib import pyplot as plt
+from straight_paper import paper_straight_on
 
-num_photos = 2
+num_photos = 4
 
 paths = [f"C:/Users/ggisl/Desktop/RotatingPapers/images/IMG_0{i}.jpg" for i in range(num_photos)]
 corners0 = [
@@ -26,47 +27,57 @@ corners2 = [
 
 ]
 
-corners = [corners0, corners1, corners2]
+corners3 = [
+    [2170, 1008],
+    [3335, 1888],
+    [932, 3359],
+    [173, 1939]
+]
 
-for i in range(num_photos):
-    img = cv.imread(paths[i])
-    img = img[:, :, ::-1]
-    plt.subplot(1, num_photos, i + 1)
-    plt.imshow(img)
+corners = [corners0, corners1, corners2, corners3]
 
-
-
-    paper_2d_corners = np.array(corners[i], dtype=np.float32)
-
-
-    paper_3d_corners = [
-        [0, 0, 0],
-        [21, 0, 0],
-        [21, 29.7, 0],
-        [0, 29.7, 0]
-    ]
-    paper_3d_corners = np.array(paper_3d_corners, dtype=np.float32)
-
-    rmse, cam_mtx, dist, rvec, tvec = calibrate(paper_3d_corners, paper_2d_corners)
-    print(rmse)
-    print(cam_mtx)
-    print(dist)
-
-    points_project = [
-        [[x, 0, 0] for x in np.linspace(0, 21, 15)] +
-        [[21, y, 0] for y in np.linspace(0, 29.7, 15)] +
-        [[x, 29.7, 0] for x in np.linspace(21, 0, 15)] +
-        [[0, y, 0] for y in np.linspace(29.7, 0, 15)] +
-        [[x, 29.7/2, 0] for x in np.linspace(0, 21, 15)] +
-        [[21/2, y, 0] for y in np.linspace(0, 29.7, 15)]
-    ][0]
-    points_project = np.array(points_project, dtype=np.float32)
-
-    points_border_img, _ = cv.projectPoints(points_project, rvec, tvec, cam_mtx, dist)
-    points_border_img = points_border_img.reshape(-1, 2)
-
-    for x, y in points_border_img:
-        plt.plot(x, y, "og", markersize=10)
+photo_ind = 3
+img = cv.imread(paths[photo_ind])
+img = img[:, :, ::-1]
+plt.subplot(1,2,1)
+plt.imshow(img)
 
 
+
+paper_2d_corners = np.array(corners[photo_ind], dtype=np.float32)
+
+
+paper_3d_corners = [
+    [0, 0, 0],
+    [21, 0, 0],
+    [21, 29.7, 0],
+    [0, 29.7, 0]
+]
+paper_3d_corners = np.array(paper_3d_corners, dtype=np.float32)
+
+rmse, cam_mtx, dist, rvec, tvec = calibrate(paper_3d_corners, paper_2d_corners)
+print(rmse)
+print(cam_mtx)
+print(dist)
+
+points_project = [
+    [[x, 0, 0] for x in np.linspace(0, 21, 15)] +
+    [[21, y, 0] for y in np.linspace(0, 29.7, 15)] +
+    [[x, 29.7, 0] for x in np.linspace(21, 0, 15)] +
+    [[0, y, 0] for y in np.linspace(29.7, 0, 15)] +
+    [[x, 29.7/2, 0] for x in np.linspace(0, 21, 15)] +
+    [[21/2, y, 0] for y in np.linspace(0, 29.7, 15)]
+][0]
+points_project = np.array(points_project)
+
+points_border_img, _ = cv.projectPoints(points_project, rvec, tvec, cam_mtx, dist)
+
+points_border_img = points_border_img.reshape(-1, 2)
+
+for x, y in points_border_img:
+    plt.plot(x, y, "og", markersize=10)
+
+plt.subplot(1,2,2)
+straight = paper_straight_on(img, paper_2d_corners, 500)
+plt.imshow(straight)
 plt.show()
---
+++ src/straight_paper.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2 as cv
+from numpy import ndarray
+
+from matrices import calibrate
+
+paper_width = 21
+paper_height = 29.7
+paper_corners_3d = np.array([
+    [0, 0, 0],
+    [paper_width, 0, 0],
+    [paper_width, paper_height, 0],
+    [0, paper_height, 0]
+], dtype=np.float32)
+
+def paper_3d_and_color(img: ndarray, paper_corners_2d: ndarray, paper_img_width: int) -> tuple:
+    rmse, cam_mtx, dist, rvec, tvec = calibrate(paper_corners_3d, paper_corners_2d)
+
+    #paper_points_3d = [[[x, y, 0] for x in range(paper_img_width + 1)] for y in range(paper_img_width * paper_height / paper_width + 1)]
+    paper_img_height = int(paper_img_width * paper_height / paper_width)
+    paper_points_3d = np.array([[[x, y, 0] for x in np.linspace(0, paper_width, paper_img_width)] for y in np.linspace(0, paper_height, paper_img_height)])
+    paper_points_3d = paper_points_3d.reshape(-1, 3)
+
+
+    projected, _ = cv.projectPoints(paper_points_3d, rvec, tvec, cam_mtx, dist)
+    projected = projected.reshape(-1, 2)
+
+    colors = np.array([img[int(y), int(x), :] for (x, y) in projected], dtype=np.uint8)
+
+    return paper_points_3d, colors
+
+def paper_straight_on(img: ndarray, paper_corners_2d: ndarray, paper_img_width: int) -> ndarray:
+    _, colors = paper_3d_and_color(img, paper_corners_2d, paper_img_width)
+
+    paper_img_height = int(paper_img_width * paper_height / paper_width)
+    colors = colors.reshape((paper_img_height, paper_img_width, 3))
+    return colors
+
+
