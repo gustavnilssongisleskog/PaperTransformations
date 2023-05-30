48a8f27f6788239888b11899ee8b54dfc18e4927
Gustav Nilsson Gisleskog <gisleskoggustav@gmail.com>
2023-05-30 18:50:34 +0200

    convex hull implemented

--- src/regions/polygons.py
+++ src/regions/polygons.py
@@ -1,10 +1,37 @@
 from typing import Tuple
 
 def convex_hull(points: list) -> list:
-    def cross(p1, p2, p3):
-        return 0
+    def clockwise(p1, p2, p3):
+        u = (p1[0] - p2[0], p1[1] - p2[1])
+        v = (p3[0] - p2[0], p3[1] - p2[1])
 
-    return []
+        return u[0] * v[1] - u[1] * v[0] > 0
+    
+    points = sorted(points)
+
+    top = points[:2]
+
+    for p in points[2:]:
+        while len(top) >= 2 and not clockwise(top[-2], top[-1], p):
+
+            if p == (0.5, 1):
+                print("ok", top)
+            top.pop()
+
+        top.append(p)
+    
+    bot = points[:-3:-1]
+
+    for p in points[-3::-1]:
+        while len(bot) >= 2 and not clockwise(bot[-2], bot[-1], p):
+            bot.pop()
+
+        bot.append(p)
+
+    top.pop()
+    bot.pop()
+
+    return top + bot
 
 def shoelace(hull: list) -> float:
     return 0
