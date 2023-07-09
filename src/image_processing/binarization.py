ab77c53cffe04edbebd693c92797e91329caa276
Gustav Nilsson Gisleskog <gisleskoggustav@gmail.com>
2023-07-09 19:53:53 +0200

    remove some unused files

--- src/image_processing/binarization.py
+++ src/image_processing/binarization.py
@@ -2,8 +2,6 @@ import numpy as np
 import cv2 as cv
 from numpy import ndarray
 
-from src.image_processing.rgb_variance import rgb_std, relative_rgb_std
-
 def grayscale(img: ndarray) -> ndarray:
     if len(img.shape) == 2:
         return img
@@ -36,44 +34,3 @@ def median_blur(img: ndarray, ksize: int = 5) -> ndarray:
     gray = grayscale(img)
 
     return cv.medianBlur(gray, ksize)
-
-
-
-def binarize_white_only(img: ndarray, variance_tolerance: float=10, thresh_func=otsu_gauss) -> ndarray:
-    std = rgb_std(img)
-    white_pixels = std < variance_tolerance
-
-    thresh_light = thresh_func(img)
-
-    return thresh_light * white_pixels
-
-
-def main():
-    from matplotlib import pyplot as plt
-    paths = [f"C:/Users/ggisl/Desktop/PaperTransformations/images/IMG_0{i}.jpg" for i in range(4)]
-    photo_ind = 3
-    for photo_ind in range(4):
-        img = cv.imread(paths[photo_ind])
-        img = img[:, :, ::-1]
-        gray = grayscale(img)
-
-        plt.subplot(2,2,1)
-        plt.imshow(img)
-
-        otsu = otsu_gauss(gray)
-
-        plt.subplot(2,2,2)
-        plt.imshow(otsu)
-
-        std = rgb_std(img)
-        plt.subplot(2,2,3)
-        plt.imshow(std)
-
-        white = binarize_white_only(img)
-        plt.subplot(2,2,4)
-        plt.imshow(white)
-
-        plt.show()
-
-if __name__ == "__main__":
-    main()
