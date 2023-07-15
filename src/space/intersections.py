16b6feb4b9cf4fa0b63f60f10d75632d0ad4a12d
Gustav Nilsson Gisleskog <gisleskoggustav@gmail.com>
2023-07-15 15:51:22 +0200

    speeding up intersection trick by 10x by making a more rigid grid and not calculating things several times

--- src/space/intersections.py
+++ src/space/intersections.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2 as cv
 
 paper_width = 21
 paper_height = 29.7
@@ -14,55 +15,73 @@ def line(a, b) -> tuple:
 
 
 def intersection_lines(l1, l2) -> np.ndarray:
-    return np.array([l1[1] * l2[2] - l2[1] * l1[2], l1[2] * l2[0] - l2[2] * l1[0]]) / (l1[0] * l2[1] - l2[0] * l1[1])
-    # return -1 / (l1[0] * l2[1] - l2[0] * l1[1]) * np.array([[l2[1], -l1[1]], [-l2[0], l1[0]]]) @ np.array([l1[2], l2[2]])
+    # return np.array([l1[1] * l2[2] - l2[1] * l1[2], l1[2] * l2[0] - l2[2] * l1[0]]) / (l1[0] * l2[1] - l2[0] * l1[1])
+    return -1 / (l1[0] * l2[1] - l2[0] * l1[1]) * np.array([[l2[1], -l1[1]], [-l2[0], l1[0]]]) @ np.array([l1[2], l2[2]])
 
     
 def intersection_coloring(img: np.ndarray, paper_corners_2d: np.ndarray, img_width: int) -> np.ndarray:
+    n = 2
+    while n < img_width:
+        n = 2 * n - 1
+        
+    img_width = n
     paper_corners_2d = np.array(paper_corners_2d, dtype=np.float64)
+
     
-    pixel_size = paper_width / img_width
+    points_2d = np.zeros((img_width, img_width, 2), dtype=np.float64)
+    points_2d[0, 0] = paper_corners_2d[0]
+    points_2d[0, -1] = paper_corners_2d[1]
+    points_2d[-1, -1] = paper_corners_2d[2]
+    points_2d[-1, 0] = paper_corners_2d[3]
+    
     
     top_vanish = intersection_lines(line(paper_corners_2d[0], paper_corners_2d[3]), line(paper_corners_2d[1], paper_corners_2d[2]))
     side_vanish = intersection_lines(line(paper_corners_2d[0], paper_corners_2d[1]), line(paper_corners_2d[2], paper_corners_2d[3]))
     
-    img_height = int(img_width * paper_height / paper_width)
     
-    ret = np.ones((img_height + 1, img_width + 1, 3), dtype=np.uint8)
-    
-    q = [(paper_corners_2d, real_paper_corners)]
+    q = [(0, img_width - 1, img_width - 1, 0)]
     
     while len(q) > 0:
-        corners_2d, corners_3d = q.pop()
-        
-        dx, dy = corners_3d[2] - corners_3d[0]
-        
-        if pixel_size > dy:
-            # color the pixels at corners_3d
-            for i in range(4):
-                ret[int(corners_3d[i, 1] / paper_height * img_height), int(corners_3d[i, 0] / paper_width * img_width)] = img[int(corners_2d[i, 1]), int(corners_2d[i, 0])]
-                
-            continue
+        top, right, bot, left = q.pop() 
         
         # create the next recursive points
-        center2d = intersection_lines(line(corners_2d[0], corners_2d[2]), line(corners_2d[1], corners_2d[3]))
-        center3d = np.array([dx, dy]) / 2 + corners_3d[0]
+        center2d = intersection_lines(line(points_2d[top, left], points_2d[bot, right]), line(points_2d[top, right], points_2d[bot, left]))
+        points_2d[(top + bot) // 2, (left + right) // 2] = center2d
         
-        top2d = intersection_lines(line(corners_2d[0], corners_2d[1]), line(top_vanish, center2d))
-        top3d = np.array([dx, 0]) / 2 + corners_3d[0]
+        if top == 0:
+            top2d = intersection_lines(line(points_2d[top, left], points_2d[top, right]), line(top_vanish, center2d))
+            points_2d[0, (left + right) // 2] = top2d
         
-        right2d = intersection_lines(line(corners_2d[1], corners_2d[2]), line(side_vanish, center2d))
-        right3d = np.array([0, dy]) / 2 + corners_3d[1]
+        if left == 0:
+            left2d = intersection_lines(line(points_2d[bot, left], points_2d[top, left]), line(side_vanish, center2d))
+            points_2d[(top + bot) // 2, 0] = left2d
         
-        bot2d = intersection_lines(line(corners_2d[2], corners_2d[3]), line(top_vanish, center2d))
-        bot3d = np.array([dx, 0]) / 2 + corners_3d[3]
+        right2d = intersection_lines(line(points_2d[top, right], points_2d[bot, right]), line(side_vanish, center2d))
+        points_2d[(top + bot) // 2, right] = right2d
         
-        left2d = intersection_lines(line(corners_2d[3], corners_2d[0]), line(side_vanish, center2d))
-        left3d = np.array([0, dy]) / 2 + corners_3d[0]
+        bot2d = intersection_lines(line(points_2d[bot, right], points_2d[bot, left]), line(top_vanish, center2d))
+        points_2d[bot, (left + right) // 2] = bot2d
         
-        q.append((np.array([corners_2d[0], top2d, center2d, left2d]), np.array([corners_3d[0], top3d, center3d, left3d])))
-        q.append((np.array([top2d, corners_2d[1], right2d, center2d]), np.array([top3d, corners_3d[1], right3d, center3d])))
-        q.append((np.array([center2d, right2d, corners_2d[2], bot2d]), np.array([center3d, right3d, corners_3d[2], bot3d])))
-        q.append((np.array([left2d, center2d, bot2d, corners_2d[3]]), np.array([left3d, center3d, bot3d, corners_3d[3]])))
+        if bot - top <= 2:
+            continue
+        
+        q.append(((top + bot) // 2, right, bot, (left + right) // 2))
+        
+        q.append(((top + bot) // 2, (left + right) // 2, bot, left))
+        
+        q.append((top, right, (top + bot) // 2, (left + right) // 2))
+        
+        q.append((top, (left + right) // 2, (top + bot) // 2, left))
+        
+    
+    ret = np.ones((img_width, img_width, 3), dtype=np.uint8)
+    for i in range(img_width):
+        for j in range(img_width):
+            x, y = points_2d[i, j]
+            ret[i, j] = img[int(y), int(x)]
+    
+    
+    img_height = int(img_width * paper_height / paper_width)
+    ret = cv.resize(ret, (img_width, img_height), interpolation=cv.INTER_AREA)
     
     return ret
